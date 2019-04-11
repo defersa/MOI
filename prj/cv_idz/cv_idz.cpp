7fb1e738a5401f5ef64826b5dd9ca68f4fd73b85
defersa <defersa@users.noreply.github.com>
2019-04-11 23:52:36 +0300

    remake otsu method
    make multi otsu methom with slider

--- prj/cv_idz/cv_idz.cpp
+++ prj/cv_idz/cv_idz.cpp
@@ -6,6 +6,162 @@
 using namespace std; 
 using namespace cv;
 
+const int sliderMax = 3;
+int slider;
+
+Mat original;
+Mat changed;
+
+void stepOtsu(vector<int> &s, int level, int levels, vector<double> &his, vector<int> &t, vector<double> &W, vector<double> &M, vector<double> &MK, double MT, double curMax, double *max) {
+	for (; s[level] < 255; s[level]++) {
+		int i = s[level];
+		W[level] += his[i];
+		MK[level] += i * his[i];
+		M[level] = MK[level] / W[level];
+
+		s[level + 1] = i + 1;
+		if (level < levels - 1) {
+			MK[level + 1] = 0;
+			W[level + 1] = 0;
+
+			stepOtsu(s, level + 1, levels, his, t, W, M, MK, MT, curMax, &*max);
+		}
+		else {
+			double Wsum = 0;
+			double MKsum = 0;
+
+			for (int k = 0; k < levels; k++) {
+				Wsum += W[k];
+				MKsum += MK[k];
+			}
+
+			W[level + 1] = 1 - Wsum;
+			MK[level + 1] = MT - MKsum;
+
+			if (MK[level + 1] <= 0) break;
+
+			M[level + 1] = MK[level + 1] / W[level + 1];
+
+			curMax = 0;
+			for (int k = 0; k < levels + 1; k++) {
+				curMax += W[k] * (M[k] - MT)*(M[k] - MT);
+			}
+			if (*max < curMax) {
+				*max = curMax;
+				for (int k = 0; k < levels; k++) {
+					t[k] = s[k];
+				}
+			}
+		}
+
+	}
+}
+
+vector<int> multiOtsu(vector<double> &his, int levels) {
+
+	vector<int> thresholds(levels, 0);
+	vector<int> s(levels + 1, 0);
+
+	vector<double> W(levels + 1, 0);
+	vector<double> M(levels + 1, 0);
+	vector<double> MK(levels + 1, 0);
+
+	double MT = 0;
+	double curMax = 0;
+	double max = 0;
+
+	for (int i = 0; i < 256; i++) {
+		MT += i * his[i];
+	}
+	stepOtsu(s, 0, levels, his, thresholds, W, M, MK, MT, curMax, &max);
+
+	return thresholds;
+}
+
+
+
+void on_trackbar(int, void*)
+{
+	int lvl = (int) slider + 1;
+
+	changed = original.clone();
+
+	vector<double> histo(256, 0);
+	int pixelCount = changed.rows*changed.cols;
+
+
+	for (int i = 0; i < changed.rows; i++) {
+		for (int j = 0; j < changed.cols; j++) {
+			double gray = changed.at<Vec3b>(i, j)[2] * 0.2126 + changed.at<Vec3b>(i, j)[1] * 0.7152 + changed.at<Vec3b>(i, j)[0] * 0.0722;
+			int intGray = floor(gray);
+			changed.at<Vec3b>(i, j)[0] = intGray;
+			changed.at<Vec3b>(i, j)[1] = intGray;
+			changed.at<Vec3b>(i, j)[2] = intGray;
+			histo[intGray] += 1;
+		}
+	}
+	
+	vector<double> histoImg = histo;
+	double maxnumber = 0;
+
+	for (int i = 0; i < 256; i++) {
+		histo[i] = histo[i] / pixelCount;
+		if (histoImg[i] > maxnumber) {
+			maxnumber = histoImg[i];
+		}
+	}
+
+	vector<int> realStep = multiOtsu(histo, lvl);
+
+	vector<int> colorOfGray(lvl, 0);
+	double step = 255 / lvl;
+
+	for (int i = 0; i < lvl; i++) {
+		colorOfGray[i] = (int)(step * (i + 1));
+	}
+
+	for (int i = 0; i < changed.rows; i++) {
+		for (int j = 0; j < changed.cols; j++) {
+			int intGray = 0;
+
+			for (int k = 0; k < lvl; k++) {
+				if (changed.at<Vec3b>(i, j)[0] < realStep[k])
+					break;
+				intGray = colorOfGray[k];
+			}
+			changed.at<Vec3b>(i, j)[0] = intGray;
+			changed.at<Vec3b>(i, j)[1] = intGray;
+			changed.at<Vec3b>(i, j)[2] = intGray;
+		}
+	}
+
+	Mat histogram(256, 256, CV_8UC3, Scalar(255, 255, 255));
+	for (int i = 0; i < 256; i++) {
+		int height = (int)histoImg[i] / maxnumber * 256;
+		rectangle(
+			histogram,
+			Point(i, 256),
+			Point(1 + i, 256 - height),
+			Scalar(128, 128, 128),
+			-1
+		);
+	}
+	for (int i = 0; i < lvl; i++) {
+		rectangle(
+			histogram,
+			Point(realStep[i], 256),
+			Point(1 + realStep[i], 1),
+			Scalar(0, 0, 0),
+			-1
+		);
+	}
+
+	imshow("Otsu", changed);
+	imshow("Histo", histogram);
+
+	moveWindow("Histo", 40, 200);
+	moveWindow("Otsu", 300, 200);
+}
 
 int otsu(double his[], double sum) {
 
@@ -54,62 +210,20 @@ int otsu(double his[], double sum) {
 int main(int argc, char** argv) 
 {
 
-	double histo[256];
+	original = cv::imread(argv[1]);
 
-	Mat inputImage = cv::imread(argv[1]);
+	/// Create Windows
+	namedWindow("Histo", 1);
+	slider = 1;
 
-	Mat image = Mat::zeros(inputImage.rows, inputImage.cols, CV_8UC3);
+	/// Create Trackbars
+	char TrackbarName[50];
+	sprintf(TrackbarName, "Levels x %d", sliderMax + 1);
 
-	for (int i = 0; i < 256; i++) {
-		histo[i] = 0;
-	}
-
-	for (int i = 0; i < inputImage.rows; i++) {
-		for (int j = 0; j < inputImage.cols; j++) {
-			double gray = inputImage.at<Vec3b>(i, j)[2] * 0.2126 + inputImage.at<Vec3b>(i, j)[1] * 0.7152 + inputImage.at<Vec3b>(i, j)[0] * 0.0722;
-			int intGray = floor(gray);
-			image.at<Vec3b>(i, j)[0] = intGray;
-			image.at<Vec3b>(i, j)[1] = intGray;
-			image.at<Vec3b>(i, j)[2] = intGray;
-			histo[intGray] += 1;
-		}
-	}
-
-	int pixelCount = image.rows*image.cols;
-	double sum = 0;
-
-	for (int i = 0; i < 256; i++) {
-		histo[i] = histo[i] / pixelCount;
-		sum += histo[i]*i;
-	}
-	
+	createTrackbar(TrackbarName, "Histo", &slider, sliderMax, on_trackbar);
+	on_trackbar(slider, 0);
 
 
-	imshow("Original", inputImage);
-
-	imshow("Gray", image);
-
-	moveWindow("Gray", 20, 20);
-	
-	int sep = otsu(histo, sum);
-
-	for (int i = 0; i < image.rows; i++) {
-		for (int j = 0; j < image.cols; j++) {
-			int intGray = 255;
-
-			if (sep > image.at<Vec3b>(i, j)[0])
-				intGray = 0;
-
-			image.at<Vec3b>(i, j)[0] = intGray;
-			image.at<Vec3b>(i, j)[1] = intGray;
-			image.at<Vec3b>(i, j)[2] = intGray;
-		}
-	}
-
-
-	imshow("Otsu", image);
-	
-	waitKey(0); 
-	
+	waitKey(0);	
 	return 0; 
 }
